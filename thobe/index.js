24fe59c0720b89b9e8afbbb99745f1c89563543c
Marouan-Elaarabi <Marouane.Elaarabi@wibitechagency.com>
2025-09-26 18:02:35 +0100

    template thobe 1

---
+++ thobe/index.js
@@ -0,0 +1,31 @@
+/** lingualearn-hero start **/
+
+function heroLogic() {
+  const swiper = new Swiper(".swiper", {
+    direction: "horizontal",
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      dynamicBullets: true,
+      clickable: true,
+    },
+    breakpoints: {
+      368: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1020: {
+        slidesPerView: 3,
+      },
+      1280: {
+        slidesPerView: 4,
+        spaceBetween: 16,
+      },
+    },
+  });
+}
+heroLogic();
+
+/** lingualearn-hero end **/
