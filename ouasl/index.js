c29f5c28149d6dd4c1fd1c7bc12719d3977f56b8
Marouan-Elaarabi <Marouane.Elaarabi@wibitechagency.com>
2025-10-01 16:56:04 +0100

    template ouasl 1

---
+++ ouasl/index.js
@@ -0,0 +1,28 @@
+/** ouasl-header start **/
+
+function ouaslHeader() {
+  const root = document.querySelector("[data-name='ouasl-header']");
+  if (!root) return;
+
+  const content = root.querySelector(".content");
+  if (!content) return;
+
+  const humbergerIcon =
+    "https://cdn.ouasl.com/assets/default-images-header-footer/hamburger-light.svg";
+  const closeIcon =
+    "https://cdn.ouasl.com/assets/default-images-header-footer/close-light.svg";
+
+  const dropDownIcon = content.querySelector(".drop-down img");
+  if (!dropDownIcon) return;
+  console.log(dropDownIcon.src);
+
+  dropDownIcon.addEventListener("click", (e) => {
+    const open = content.classList.contains("open");
+    !open ? (dropDownIcon.src = closeIcon) : (dropDownIcon.src = humbergerIcon);
+    content.classList.toggle("open");
+  });
+}
+
+ouaslHeader();
+
+/** ouasl-header end **/
