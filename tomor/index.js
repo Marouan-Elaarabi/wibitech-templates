393276dd015f335c851d1da7a667f11d4a04501f
Marouan-Elaarabi <Marouane.Elaarabi@wibitechagency.com>
2025-10-07 09:12:04 +0100

    tomor 2

--- tomor/index.js
+++ tomor/index.js
@@ -25,3 +25,30 @@ function tomorHeader() {
 tomorHeader();
 
 /** tomor-header end **/
+
+/** tomor-testimonials start **/
+
+function testimonialsLogic() {
+  const swiper = new Swiper(".swiper", {
+    direction: "horizontal",
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      dynamicBullets: true,
+      clickable: true,
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+        spaceBetween: 60,
+      },
+      1200: {
+        slidesPerView: 3,
+        spaceBetween: 60,
+      },
+    },
+  });
+}
+testimonialsLogic();
+
+/** tomor-testimonials end **/
