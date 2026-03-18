34f957b662826c05c4eaf8acae912267f65e58cb
Marouan-Elaarabi <Marouane.Elaarabi@wibitechagency.com>
2026-03-18 07:34:42 +0000

    syncing

--- section layout/code/demand/demand 1/index.js
+++ section layout/code/demand/demand 1/index.js
@@ -1,16 +1,8 @@
+/** seclayout-demand-1 js start **/
+
 (function () {
   const section = document.querySelector("#seclayout-demand-1");
   if (!section) return;
-
-  const offers = section.querySelectorAll(".demand-offer");
-
-  offers.forEach(function (offer) {
-    offer.addEventListener("click", function () {
-      offers.forEach(function (o) {
-        o.classList.remove("active");
-      });
-      offer.classList.add("active");
-      offer.querySelector('input[type="radio"]').checked = true;
-    });
-  });
 })();
+
+/** seclayout-demand-1 js end **/
